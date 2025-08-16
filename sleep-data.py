fb97b1c0e7b688e2acaa39b2024387d50ae3883c
Carlos González Sánchez <carlosgosa94@gmail.com>
2025-08-16 19:40:38 +0200

    Update sleep-data.py
    Prueba para añadir puntuacion de sueño

--- sleep-data.py
+++ sleep-data.py
@@ -73,7 +73,8 @@ def create_sleep_data(client, database_id, sleep_data, skip_zero_sleep=True):
         "Deep Sleep": {"rich_text": [{"text": {"content": format_duration(daily_sleep.get('deepSleepSeconds', 0))}}]},
         "REM Sleep": {"rich_text": [{"text": {"content": format_duration(daily_sleep.get('remSleepSeconds', 0))}}]},
         "Awake Time": {"rich_text": [{"text": {"content": format_duration(daily_sleep.get('awakeSleepSeconds', 0))}}]},
-        "Resting HR": {"number": sleep_data.get('restingHeartRate', 0)}
+        "Resting HR": {"number": sleep_data.get('restingHeartRate', 0)},
+        "Score": {"number": sleep_data.get('sleepScores', {}).get('overall', {}).get('value', None)}
     }
     
     client.pages.create(parent={"database_id": database_id}, properties=properties, icon={"emoji": "😴"})
